adae572d7810f732baaf70b64f73077f71c4d699
aryanatel <atelaryan@gmail.coom>
2024-06-04 13:18:38 +0530

    Java Code change

--- src/main/java/com/example/springboot/controller/SpringController.java
+++ src/main/java/com/example/springboot/controller/SpringController.java
@@ -23,7 +23,7 @@ public class SpringController {
 //		Integer sec = date.getSecond();
 		val++;
 //		Integer count = year + month + day + hour + min + sec + 1;
-		String result = "Mr. DevOps" + val;
+		String result = "Mr. Atel" + val;
 		return result;
 	}
 }
